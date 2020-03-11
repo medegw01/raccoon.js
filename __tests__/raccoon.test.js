34b451f59236d6dabe4caefab1bcb0ad62ce2f3f
medegw01 <michael.edegware@tufts.edu>
2020-03-11 16:53:55 -0400

    changes perft test

--- __tests__/raccoon.test.js
+++ __tests__/raccoon.test.js
@@ -7,14 +7,14 @@ if (typeof require != "undefined") {
 }
 describe("Perft", function() {
     let perfts = [
-        {fen: 'r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1',
-            depth: 3, nodes: 97862},
-        {fen: '8/PPP4k/8/8/8/8/4Kppp/8 w - - 0 1',
-            depth: 4, nodes: 89363},
+        {fen: 'r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1',
+            depth: 5, nodes: 15833292},
+        {fen: 'r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10',
+            depth: 5, nodes: 164075551},
         {fen: '8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1',
             depth: 4, nodes: 43238},
-        {fen: 'rnbqkbnr/p3pppp/2p5/1pPp4/3P4/8/PP2PPPP/RNBQKBNR w KQkq b6 0 4',
-            depth: 3, nodes: 23509},
+        {fen: 'rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8',
+            depth: 5, nodes: 89941194 },
     ];
 
     perfts.forEach(function(perft) {
